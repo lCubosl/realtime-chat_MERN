d3eab14fe5065d88a09615767c3e668c72bf696b
LuisM <muislarques@gmail.com>
2025-05-23 17:33:28 +0100

    routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 // framework, provides features that allows us to build the API faster. gives us routes, middleware, etc...
 import express from "express"
-import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv"
 
+import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js"
+
+dotenv.config()
 const app = express()
+const PORT = process.env.PORT
 
 app.use("/api/auth", authRoutes)
 
-app.listen(5001, () => {
-  console.log("server is running on port 5001")
+app.listen(PORT, () => {
+  console.log("server is running on port:", PORT)
+  connectDB()
 })
