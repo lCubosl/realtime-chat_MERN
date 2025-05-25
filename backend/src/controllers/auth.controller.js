3afa0439c256fc356b941d999d2e06eedf2e9120
LuisM <muislarques@gmail.com>
2025-05-25 02:07:35 +0100

    routes for login and logout; updates to signup and comments for readability

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { generateToken } from "../lib/utils.js"
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 
+// signup route
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body
   try {
@@ -9,16 +10,16 @@ export const signup = async (req, res) => {
     if (!fullName || !email || !password) {
       return res.status(400).json({ message: "All the fields are required" })
     }
-
-    // password hashing to "encrypt passwords"
+    
     if (password.length < 6) {
       return res.status(400).json({message: "Password must be at least 6 characters long"})
     }
     
     const user = await User.findOne({email})
-
-    if (user) return res.status(400).json({message: "email already exists"})
     
+    if (user) return res.status(400).json({message: "email already exists"})
+      
+    // password hashing to "encrypt passwords"
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password, salt)
 
@@ -47,10 +48,43 @@ export const signup = async (req, res) => {
   }
 }
 
-export const login = (req, res) => {
-  res.send("login route")
+// login route
+export const login = async (req, res) => {
+  const { email, password } = req.body
+  try {
+    const user = await User.findOne({ email })
+
+    if(!user) {
+      return res.status(400).json({message:"Invalid user or password"})
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+    if(!isPasswordCorrect) {
+      return res.status(400).json({message:"Invalid user or password"})
+    }
+
+    generateToken(user._id, res)
+
+    res.status(200).json({
+      _id:user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    })
+  } catch (error) {
+    console.log("Error in login controller", error.message)
+    res.status(500).json({ message: "Internal Server Error" })
+  }
 }
 
+// logout route
 export const logout = (req, res) => {
-  res.send("logout route")
+  try {
+    res.cookie("jwt", "", {maxAge:0})
+    res.status(200).json({ message: "Logged out successfully" })
+  } catch (error) {
+    console.log("Error in loggout controller", error.message)    
+    res.status(500).json({ message: "Internal Server Error" })
+
+  }
 }
