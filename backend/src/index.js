88f4203c23a8384f26b47ab1a4b737ef820a61c9
LuisM <muislarques@gmail.com>
2025-05-26 22:42:08 +0100

    message.controller, message.routes, update index with new routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,9 @@ import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 
-import authRoutes from "./routes/auth.route.js"
 import { connectDB } from "./lib/db.js"
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 
 dotenv.config()
 const app = express()
@@ -14,6 +15,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT, () => {
   console.log("server is running on port:", PORT)
