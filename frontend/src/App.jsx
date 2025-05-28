83978c67af08a5e7e77cde904beea13331bafed9
LuisM <muislarques@gmail.com>
2025-05-28 17:12:38 +0100

    settings customization update color pallette

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import ProfilePage from "./pages/ProfilePage";
 
 import { Routes, Route, Navigate } from "react-router-dom"
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 import { useEffect } from "react";
 
 import { Loader } from "lucide-react"
@@ -14,6 +15,7 @@ import { Toaster } from "react-hot-toast"
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
+  const { theme } = useThemeStore()
 
   useEffect(() => {
     checkAuth()
@@ -28,7 +30,7 @@ const App = () => {
   )
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
