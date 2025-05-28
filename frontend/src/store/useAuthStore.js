d24f09757d7f4b7cd236e20ed15b3c58a6c37296
LuisM <muislarques@gmail.com>
2025-05-28 15:36:45 +0100

    loggin, logot, update authentication

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -37,4 +37,28 @@ export const useAuthStore = create((set) => ({
       set({ isSigningUp:false })
     }
   },
+
+  login: async(data) => {
+    set({ isLoggingIn:true })
+    try {
+      const res = await axiosInstance.post("/auth/login", data)
+      set({ authUser:res.data })
+      toast.success("Logged In successfully")
+    
+    } catch (error) {
+      toast.error(error.response.data.message)      
+    } finally {
+      set({ isLoggingIn:false })
+    }
+  },
+
+  logout: async() => {
+    try {
+      await axiosInstance.post("/auth/logout")
+      set({ authUser:null })
+      toast.success("Logged out successfully")
+    } catch (error) {
+      toast.error(error.response.data.message)      
+    }
+  }
 }))
