c49954e4b30738f77380de91d98da7bb9a8955bd
LuisM <muislarques@gmail.com>
2025-05-28 11:46:35 +0100

    login front end integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 
 import { Loader } from "lucide-react"
+import { Toaster } from "react-hot-toast"
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
@@ -32,11 +33,13 @@ const App = () => {
 
       <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage />  : <Navigate to="/login" />} />
       </Routes>
+
+      <Toaster />
     </div>
   );
 }
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand"
 import { axiosInstance } from "../lib/axios.js"
+import toast from "react-hot-toast"
 
 export const useAuthStore = create((set) => ({
   authUser: null,
@@ -13,7 +14,7 @@ export const useAuthStore = create((set) => ({
     try {
       const res = await axiosInstance.get("/auth/check")
       set({authUser:res.data})
-      
+
     } catch (error) {
       console.log("Error in checkAuth:", error)
       set({authUser:null})
@@ -21,5 +22,19 @@ export const useAuthStore = create((set) => ({
     } finally {
       set({isCheckingAuth:false})
     }
-  }
+  },
+
+  signup: async(data) => {
+    set({ isSigningUp:true  })
+    try {
+      const res = await axiosInstance.post("auth/signup", data)
+      set({ authUser: res.data })
+      toast.success("Account created successfully")
+
+    } catch (error) {
+      toast.error(error.response.data.message)
+    } finally {
+      set({ isSigningUp:false })
+    }
+  },
 }))
