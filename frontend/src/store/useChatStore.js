3d8c9159c360f38e7014a93807896f52b71b2111
LuisM <muislarques@gmail.com>
2025-05-29 23:50:21 +0100

    chat working

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import {create} from "zustand"
 import toast from "react-hot-toast"
 import { axiosInstance } from "../lib/axios"
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -21,7 +21,7 @@ export const useChatStore = create((set) => ({
     }
   },
 
-  getMessages: async(userId) => {
+  getMessages: async (userId) => {
     set({isMessagesLoading:true})
     try {
       const res = await axiosInstance.get(`/messages/${userId}`)
@@ -30,6 +30,16 @@ export const useChatStore = create((set) => ({
       toast.error("error loading messages", error.response.data.message)      
     } finally {
       set({isMessagesLoading:false})
+    }
+  },
+
+  sendMessage: async (messageData) => {
+    const {selectedUser, messages} = get()
+    try {
+      const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData)
+      set({messages: [...messages, res.data]})     
+    } catch (error) {
+      toast.error("error in sending message", error.response.data.message)      
     }
   },
 
