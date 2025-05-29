eb2921be162107dac656177969165da75d39acbe
LuisM <muislarques@gmail.com>
2025-05-29 17:51:02 +0100

    chat containe, chat header

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,17 +1,35 @@
 import { useEffect } from "react"
 import { useChatStore } from "../store/useChatStore"
 
+import ChatHeader from "./ChatHeader"
+import MessageInput from "./MessageInput"
+import MessageSkeleton from "./skeletons/MessageSkeleton"
+
 const ChatContainer = () => {
   const {messages, getMessages, isMessagesLoading, selectedUser} = useChatStore()
 
   useEffect(() => {
+    getMessages(selectedUser._id)
+  }, [selectedUser._id, getMessages])
 
-  }, [])
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  }
 
   if(isMessagesLoading) return <div>Loading...</div>
 
   return (
-    <div>ChatContainer</div>
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <p>messages...</p>
+      <MessageInput />      
+    </div>
   )
 }
 
