dc0a0414298ac7b8b46d1ddb3d0795965410f5bf
LuisM <muislarques@gmail.com>
2025-05-31 22:16:50 +0100

    trying to fix typos

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -17,8 +17,8 @@ export const useAuthStore = create((set, get) => ({
   checkAuth: async() => {
     try {
       const res = await axiosInstance.get("/auth/check")
+      
       set({authUser:res.data})
-
       get().connectSocket()
 
     } catch (error) {
